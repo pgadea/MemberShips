e68e738d07f951f5e4516c6f6c5fa35d38dbfc9a
Pierre <pgadea7@gmail.com>
2018-05-13 19:40:29 -0500

    Added Product Controller and ConversionExtension method. Added ProdcutModel and Views.

--- Memberships/Areas/Admin/Models/SmallButtonModel.cs
+++ Memberships/Areas/Admin/Models/SmallButtonModel.cs
@@ -30,16 +30,16 @@ namespace Memberships.Areas.Admin.Models
             {
                 var param = new StringBuilder("?");
                 if (Id != null && Id > 0)
-                    param.Append(String.Format("{0}={1}&", "id", Id));
+                    param.Append(string.Format("{0}={1}&", "id", Id));
 
                 if (ItemId != null && ItemId > 0)
-                    param.Append(String.Format("{0}={1}&", "itemId", ItemId));
+                    param.Append(string.Format("{0}={1}&", "itemId", ItemId));
 
                 if (ProductId != null && ProductId > 0)
-                    param.Append(String.Format("{0}={1}&", "productId", ProductId));
+                    param.Append(string.Format("{0}={1}&", "productId", ProductId));
 
                 if (SubscriptionId != null && SubscriptionId > 0)
-                    param.Append(String.Format("{0}={1}&", "subscriptionId", SubscriptionId));
+                    param.Append(string.Format("{0}={1}&", "subscriptionId", SubscriptionId));
 
                 return param.ToString().Substring(0, param.Length - 1);
             }
